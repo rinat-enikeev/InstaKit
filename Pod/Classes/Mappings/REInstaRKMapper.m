9de4821bfb16d6ad06740a4e0c5791df9b0021b1
Rinat Enikeev <rinat.enikeev@gmail.com>
2015-11-17 22:53:25 +0500

    InstaPostDeep mapping with images and user

--- Pod/Classes/Mappings/REInstaRKMapper.m
+++ Pod/Classes/Mappings/REInstaRKMapper.m
@@ -4,24 +4,132 @@
 //
 //  Created by Rinat Enikeev on 17/11/15.
 //
-//
+//  http://stackoverflow.com/questions/16395925/restkit-map-values-from-nested-dictionary
+
 
 #import "REInstaRKMapper.h"
 #import "REInstaPost.h"
+#import "REInstaUser.h"
+#import "REInstaImage.h"
 #import <RestKit/RestKit.h>
+
+@interface REInstaRKMapper()
+@property (nonatomic, strong) RKManagedObjectStore* store;
+@property (nonatomic, strong) RKEntityMapping* postDeep;
+@property (nonatomic, strong) RKEntityMapping* user; //
+@property (nonatomic, strong) RKEntityMapping* image; //
+@end
 
 @implementation REInstaRKMapper
 
--(void)mapPostDeep {
-    RKObjectMapping* articleMapping = [RKObjectMapping mappingForClass:[REInstaPost class]];
-    [articleMapping addAttributeMappingsFromDictionary:@{
-                                                         @"title": @"title",
-                                                         @"body": @"body",
-                                                         @"author": @"author",
-                                                         @"publication_date": @"publicationDate"
-                                                         }];
-    
-    RKResponseDescriptor *responseDescriptor = [RKResponseDescriptor responseDescriptorWithMapping:articleMapping method:RKRequestMethodAny pathPattern:nil keyPath:@"articles" statusCodes:RKStatusCodeIndexSetForClass(RKStatusCodeClassSuccessful)];
+-(id)initWithRKManagedObjectStore:(RKManagedObjectStore*)store {
+    self = [super init];
+    if (self) {
+        self.store = store;
+    }
+    return self;
 }
+
+-(void)mapPostDeep {
+    
+    self.postDeep = [RKEntityMapping mappingForEntityForName:NSStringFromClass([REInstaPost class]) inManagedObjectStore:_store];
+    
+    // 1. Attributes
+    [_postDeep addAttributeMappingFromKeyOfRepresentationToAttribute:@"comments"];
+    [_postDeep addAttributeMappingFromKeyOfRepresentationToAttribute:@"likes"];
+    
+    [_postDeep addAttributeMappingsFromDictionary:
+        @{
+         @"id"             : @"identifier",
+         @"comments.count" : @"commentsCount",
+         @"likes.count"    : @"likesCount",
+         @"created_time"   : @"createdTimestamp",
+         @"filter"         : @"filter",
+         @"link"           : @"link",
+         @"type"           : @"type",
+         @"user_has_liked" : @"userHasLiked",
+         @"distance"       : @"distance"
+         }];
+    
+    [_postDeep setIdentificationAttributes:@[@"identifier"]];
+    
+    // 2. Relationships
+    RKRelationshipMapping* autorRel = [RKRelationshipMapping relationshipMappingFromKeyPath:@"user" toKeyPath:@"author" withMapping:_user];
+    [_postDeep addPropertyMapping:autorRel];
+    
+    
+    RKRelationshipMapping* lowImageMapping = [RKRelationshipMapping relationshipMappingFromKeyPath:@"images.low_resolution" toKeyPath:@"imageLow" withMapping:_image];
+    [_postDeep addPropertyMapping:lowImageMapping];
+    
+    RKRelationshipMapping* thumbImageMapping = [RKRelationshipMapping relationshipMappingFromKeyPath:@"images.thumbnail" toKeyPath:@"imageThumb" withMapping:_image];
+    [_postDeep addPropertyMapping:thumbImageMapping];
+    
+    RKRelationshipMapping* stdImageMapping = [RKRelationshipMapping relationshipMappingFromKeyPath:@"images.standard_resolution" toKeyPath:@"imageStd" withMapping:_image];
+    [_postDeep addPropertyMapping:stdImageMapping];
+    
+    
+    
+}
+
+// TODO mapPostDeep
+//@property (nullable, nonatomic, retain) NSSet<REInstaUser *> *likers;
+//@property (nullable, nonatomic, retain) NSSet<REInstaTag *> *tags;
+//@property (nullable, nonatomic, retain) REInstaVideo *videoLowBand;
+//@property (nullable, nonatomic, retain) REInstaVideo *videoLowRes;
+//@property (nullable, nonatomic, retain) REInstaVideo *videoStdRes;
+//@property (nullable, nonatomic, retain) REInstaAttribution *attribution;
+//@property (nullable, nonatomic, retain) NSSet<REInstaUserOnPost *> *usersOnPost;
+
+
+-(void)mapUser {
+    self.user = [RKEntityMapping mappingForEntityForName:NSStringFromClass([REInstaUser class]) inManagedObjectStore:_store];
+    
+    [_user addAttributeMappingFromKeyOfRepresentationToAttribute:@"counts"];
+    
+    [_user addAttributeMappingsFromDictionary:
+     @{
+       @"id"              : @"identifier",
+       @"username"        : @"username",
+       @"first_name"      : @"firstName",
+       @"last_name"       : @"lastName",
+       @"full_name"       : @"fullName",
+       @"bio"             : @"bio",
+       @"profile_picture" : @"profilePictureUrl",
+       @"website"         : @"website",
+       @"counts.follows"     : @"followsCount",
+       @"counts.followed_by" : @"followedByCount",
+       @"counts.media"       : @"mediaCount"
+       }];
+    
+    [_user setIdentificationAttributes:@[@"identifier"]];
+}
+
+// TODO mapUser
+//@property (nullable, nonatomic, retain) NSSet<REInstaCaption *> *captions;
+//@property (nullable, nonatomic, retain) NSSet<REInstaComment *> *comments;
+//@property (nullable, nonatomic, retain) NSSet<REInstaPost *> *likes;
+//@property (nullable, nonatomic, retain) NSSet<REInstaPost *> *posts;
+//@property (nullable, nonatomic, retain) NSSet<REInstaRelationship *> *relationships;
+//@property (nullable, nonatomic, retain) NSSet<REInstaRelationship *> *relationshipRequests;
+//@property (nullable, nonatomic, retain) NSSet<REInstaUserOnPost *> *onPostPhotos;
+
+
+-(void)mapImage {
+    self.image = [RKEntityMapping mappingForEntityForName:NSStringFromClass([REInstaImage class]) inManagedObjectStore:_store];
+    
+    [_image addAttributeMappingsFromDictionary:
+     @{
+       @"height" : @"height",
+       @"url"    : @"url",
+       @"width"  : @"width"
+       }];
+}
+
+// TODO mapImage
+//@property (nullable, nonatomic, retain) NSSet<REInstaPost *> *postLow;
+//@property (nullable, nonatomic, retain) NSSet<REInstaPost *> *postStd;
+//@property (nullable, nonatomic, retain) NSSet<REInstaPost *> *postThumb;
+
+
 
 @end
